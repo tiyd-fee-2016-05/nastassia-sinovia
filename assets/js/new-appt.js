4a98dce29a680a1aab8cec44f83f90332e6d6fd2
SINOVIA <somayfield@gmail.com>
2016-06-23 16:59:35 -0400

    Updated

--- assets/js/new-appt.js
+++ assets/js/new-appt.js
@@ -1,4 +1,10 @@
+var submitCount = 0
+
 $(".saveNew").click(function(){
+
+submitCount++
+
+var appointmentID = submitCount
 
 $(".addAppt").submit();
   console.log ("Form submitted!")
@@ -6,6 +12,36 @@ $(".addAppt").submit();
 var appointmentTitle = $('input[id="appTitle"]').val();
 
 console.log(appointmentTitle);
+
+var appointmentStreet = $('input[id="appStreet"]').val();
+
+console.log(appointmentStreet);
+
+var appointmentLocation = $('input[id="appLocation"]').val();
+
+console.log(appointmentLocation);
+
+var appointmentDate = $('input[id="appDate"]').val();
+
+console.log(appointmentDate);
+
+var appointmentTime = $('input[id="appTime"]').val();
+
+console.log(appointmentTime);
+
+
+localStorage.setItem('appointment', JSON.stringify({
+    AppointmentID: appointmentID,
+    Title: appointmentTitle,
+    Street: appointmentStreet,
+    CityState: appointmentLocation,
+    Day: appointmentDate,
+    Time: appointmentTime,
+
+
+}));
+
+
 
 
 
