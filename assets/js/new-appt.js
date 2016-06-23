c26bef993d8169168a1dc837961171a52cc0de16
SINOVIA <somayfield@gmail.com>
2016-06-23 16:33:45 -0400

    Updated

---
+++ assets/js/new-appt.js
@@ -0,0 +1,12 @@
+$(".saveNew").click(function(){
+
+$(".addAppt").submit();
+  console.log ("Form submitted!")
+
+var appointmentTitle = $('input[id="appTitle"]').val();
+
+console.log(appointmentTitle);
+
+
+
+});
