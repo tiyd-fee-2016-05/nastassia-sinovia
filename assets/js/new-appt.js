26e038aef8c069c646074b735c8cbdf2de974ace
SINOVIA <somayfield@gmail.com>
2016-06-24 12:32:14 -0400

    Add Appointment

--- assets/js/new-appt.js
+++ assets/js/new-appt.js
@@ -4,33 +4,37 @@ $(".saveNew").click(function(){
 
 submitCount++
 
+
 var appointmentID = submitCount
 
-$(".addAppt").submit();
-  console.log ("Form submitted!")
-
+console.log(appointmentID)
+//
+// Turns User Input for Appointment Title into Variable for Local Storage Call
 var appointmentTitle = $('input[id="appTitle"]').val();
-
 console.log(appointmentTitle);
-
+//
+//
+// Turns User Input for Appointment Street into Variable for Local Storage Call
 var appointmentStreet = $('input[id="appStreet"]').val();
-
 console.log(appointmentStreet);
-
+//
+//
+// Turns User Input for Appointment Location into Variable for Local Storage Call
 var appointmentLocation = $('input[id="appLocation"]').val();
-
 console.log(appointmentLocation);
-
+//
+//
+// Turns User Input for Appointment Date into Variable for Local Storage Call
 var appointmentDate = $('input[id="appDate"]').val();
-
 console.log(appointmentDate);
-
+//
+//
+// // Turns User Input for Appointment Time into Variable for Local Storage Call
 var appointmentTime = $('input[id="appTime"]').val();
-
 console.log(appointmentTime);
-
-
-localStorage.setItem('appointment', JSON.stringify({
+//
+//
+localStorage.setItem('appointment'+ appointmentID, JSON.stringify({
     AppointmentID: appointmentID,
     Title: appointmentTitle,
     Street: appointmentStreet,
