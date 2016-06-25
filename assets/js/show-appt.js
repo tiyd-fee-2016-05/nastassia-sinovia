bb3d906739cf74e36b09d4510597d2aa4a370c05
SINOVIA <somayfield@gmail.com>
2016-06-25 17:09:08 -0400

    Appointments show

--- assets/js/show-appt.js
+++ assets/js/show-appt.js
@@ -25,6 +25,72 @@ $(".appointmentList").append(
 
 console.log( "Loaded Appointment 3!" );
 
+// ____________End Item 3________________________
+var getAppointment4= JSON.parse(localStorage.getItem('appointment4'));
+
+$(".appointmentList").append(
+"<li class='appt4'>" +"<p class='timeBlock'>" + getAppointment4.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment4.Title + '</h1>' + '  <h1 class="location">' + getAppointment4.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 4!" );
+// ____________End Item 4________________________
+var getAppointment5= JSON.parse(localStorage.getItem('appointment5'));
+
+$(".appointmentList").append(
+"<li class='appt5'>" +"<p class='timeBlock'>" + getAppointment5.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment5.Title + '</h1>' + '  <h1 class="location">' + getAppointment5.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 5!" );
+// ____________End Item 5________________________
+var getAppointment6= JSON.parse(localStorage.getItem('appointment6'));
+
+$(".appointmentList").append(
+"<li class='appt6'>" +"<p class='timeBlock'>" + getAppointment6.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment6.Title + '</h1>' + '  <h1 class="location">' + getAppointment6.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 6!" );
+// ____________End Item 6________________________
+var getAppointment6= JSON.parse(localStorage.getItem('appointment6'));
+
+$(".appointmentList").append(
+"<li class='appt7'>" +"<p class='timeBlock'>" + getAppointment7.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment7.Title + '</h1>' + '  <h1 class="location">' + getAppointment7.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 7!" );
+// ____________End Item 7________________________
+var getAppointment8= JSON.parse(localStorage.getItem('appointment8'));
+
+$(".appointmentList").append(
+"<li class='appt8'>" +"<p class='timeBlock'>" + getAppointment8.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment8.Title + '</h1>' + '  <h1 class="location">' + getAppointment8.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 8!" );
+// ____________End Item 8________________________
+var getAppointment9= JSON.parse(localStorage.getItem('appointmen9'));
+
+$(".appointmentList").append(
+"<li class='appt9'>" +"<p class='timeBlock'>" + getAppointment9.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment9.Title + '</h1>' + '  <h1 class="location">' + getAppointment9.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 9!" );
+// ____________End Item 7________________________
+var getAppointment9= JSON.parse(localStorage.getItem('appointmen9'));
+
+$(".appointmentList").append(
+"<li class='appt10'>" +"<p class='timeBlock'>" + getAppointment10.Time + '</p>' + "<span='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment10.Title + '</h1>' + '  <h1 class="location">' + getAppointment10.CityState+ '</h1>' + '</span>' + '</li>')
+
+console.log( "Loaded Appointment 10!" );
+// ____________End Item 7________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ____________Loads Appointment 1 Into Modal________________________
 $(".appt1").click(function() {
 $(".detailsModal1").css("display", "block")
