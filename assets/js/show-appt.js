7a074b9e0d9a484f966ce72668560acf957ce8a6
SINOVIA <somayfield@gmail.com>
2016-06-25 16:20:35 -0400

    Appointment 1 loads into Modal

--- assets/js/show-appt.js
+++ assets/js/show-appt.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
   var getAppointment1= JSON.parse(localStorage.getItem('appointment1'));
 
 $(".appointmentList").append(
-  "<li class='appt1'>" +"<p class='timeBlock'>" + getAppointment1.Time + '</p>' + "<a href='details-appt.html' class='titleBlock'><h1 class='title'>" + getAppointment1.Title + '</h1>' + '  <h1 class="location">' + getAppointment1.CityState+ '</h1>' + '</a>' + '</li>')
+  "<li class='appt1'>" +"<p class='timeBlock'>" + getAppointment1.Time + '</p>' + "<span class='titleBlock'><h1 class='title'>" + getAppointment1.Title + '</h1>' + '  <h1 class="location">' + getAppointment1.CityState+ '</h1>' + '</span>' + '</li>')
 
   console.log( "Loaded Appointment 1!" );
 
@@ -25,6 +25,24 @@ $(".appointmentList").append(
 
 console.log( "Loaded Appointment 3!" );
 
+// ____________Loads Appointment 1 Into Modal________________________
+$(".appt1").click(function() {
+$(".detailsModal").css("display", "block")
+
+
+    var getAppointment1= JSON.parse(localStorage.getItem('appointment1'));
+
+  $(".detailsHeader").append('<h1 class="detailsLogoTitle"><i class="fa fa-chevron-left closeDetailsModal" aria-hidden="true"></i>' + getAppointment1.Title + '</h1>')
+  $(".appointmentDetail").append('<h1>' + getAppointment1.Day + ' at ' + '<span class="detailsTime">' + getAppointment1.Time + '</span></h1><h1 class="detailsLocation">' + getAppointment1.CityState + '<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i></h1>' + ' <img src="assets/images/map.png">' + '<h1 class="weather">' + 'Rainy, 55F' + '</h1>')
+});
+
+$(".closeDetailsModal").click(function () {
+$(".detailsModal").css("display", "none");
+console.log("Clicked")
+});
+
+});
+
 // ____________End Item 3________________________
 // var getAppointment4= JSON.parse(localStorage.getItem('appointment4'));
 //
@@ -35,24 +53,21 @@ console.log( "Loaded Appointment 3!" );
 //
 // // ____________End Item 4________________________
 
-});
 
-
-$(".appt1").click(function() {
-  e.preventDefault();
+// var getAppointment1= JSON.parse(localStorage.getItem('appointment1'));
+//
+// $(".detailsModal").append('<header class ="detailsHeader">' + '<h1 class="detailsLogoTitle"><a href="index.html"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>' + getAppointment1.Title + '</h1></header>')
+//
+//
+// console.log( "Loaded Appointment 1 to Modal!" );
+//
+//
+// $(".appt1").click(function() {
+//   e.preventDefault();
 
   //   var getAppointment1= JSON.parse(localStorage.getItem('appointment1'));
   //
   // $(".detailsHeader").append('<h1 class="detailsLogoTitle"><a href="index.html"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>' + getAppointment1.Title + '</h1>')
 
 
-    console.log("You clicked Meet with Ash");
-
-
     // $(".detailsHeader").append('<header><h1 class="detailsLogoTitle"><a href="index.html"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>' + getAppointment1.Title + '</h1></header><div class="appointmentDetail"><h1>' + getAppointment1.Date + ' at ' + '<span class="detailsTime">' + getAppointment1.Time + '</span></h1><h1 class="detailsLocation">' + getAppointment1.CityState + '<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i></h1> <img src="assets/images/map.png">' + ' <h1 class="weather"> Rainy, 55F</h1>' + '</div>' + '<button class="editButton" type="submit"><a href="edit-appt.html">Edit Appointment</a></button>')
-
-
-
-
-
-})
