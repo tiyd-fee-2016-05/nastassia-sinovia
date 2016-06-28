18988fab87c66570068a321c02f39acecfb69441
SINOVIA <somayfield@gmail.com>
2016-06-28 14:15:57 -0400

    Appointments Loading in Angular!

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,12 +25,14 @@ mainApp.config(function($routeProvider) {
 });
 
 mainApp.controller('AppointmentController', function($scope){
-  $scope.students = [
-    {name: 'Mark Waugh', city: 'New York'},
-    {name: 'Steve Jonathan', city: 'London'},
-    {name: 'John Marcus', city: 'Paris'}
-  ];
+  $scope.appointment =
+[
+  {AppointmentID: '1', Title:'Meet with Ash',Street:'42 Friend St.', CityState:'Durham', Day:'9-12-2016',Time: '3pm'},
 
+  {AppointmentID: '2', Title:'Lunch with Misty',Street:'123 Brunch Ave.', CityState:'Little Rock', Day:'9-12-2016',Time: '3pm'},
+
+  {AppointmentID: '3', Title:'Brunch with Brock',Street:'42 Friend St.', CityState:'Raleigh', Day:'9-12-2016',Time: '3pm'},
+]
   $scope.message = "This is a test message."
 
 });
